bf5c197baf4e3c52087e8f0fe074211bc40cddd5
Shrekshreker42069 <shrekshreker42069@gmail.com>
2017-01-19 08:58:32 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include <vector>
 
 using namespace std;
-char fap_map[18][32];
+char dab_map[18][32];
 
 char map[18][32] = {
 	" _____________________________ ",
@@ -77,7 +77,7 @@ private:
 };
 
 struct walk {
-	short walk_count;
+	short sanic_count;
 	short x;
 	short y;
 	short back;
@@ -88,41 +88,41 @@ struct target {
 	short y;
 };
 
-vector<target> walk_queue;
+vector<target> sanic_speed;
 
 vector<walk> MEXArray;
 
 void AddArray( int x, int y, int wc , int back ){
-	if( fap_map[y][x] == ' ' || fap_map[y][x] == '.' ){
-		fap_map[y][x] = '#';
-		walk fap;
-		fap.x = x;
-		fap.y = y;
-		fap.walk_count = wc;
-		fap.back = back;
-		MEXArray.push_back( fap );
+	if( dab_map[y][x] == ' ' || dab_map[y][x] == '.' ){
+		dab_map[y][x] = '#';
+		sanic dab;
+		dab.x = x;
+		dab.y = y;
+		dab.sanic_count = wc;
+		dab.back = back;
+		MEXArray.push_back( dab );
 	}
 }
 
-void FindPath( int sx, int sy, int x, int y ){
-	memcpy( fap_map, map, sizeof(map) );
+void FindNemo( int sx, int sy, int x, int y ){
+	memcpy( dab_map, map, sizeof(map) );
 	MEXArray.clear();
-	walk fap;
-	fap.x = sx;
-	fap.y = sy;
-	fap.walk_count = 0;
-	fap.back = -1;
-	MEXArray.push_back( fap );
+	sanic dab;
+	dab.x = sx;
+	dab.y = sy;
+	dab.sanic_count = 0;
+	dab.back = -1;
+	MEXArray.push_back( dab );
 
 	int i = 0;
 	while( i < MEXArray.size() ){
 		if( MEXArray[i].x == x && MEXArray[i].y == y ){
-			walk_queue.clear();
-			target fap2;
-			while( MEXArray[i].walk_count != 0 ){
-				fap2.x = MEXArray[i].x;
-				fap2.y = MEXArray[i].y;
-				walk_queue.push_back( fap2 );
+			sanic_speed.clear();
+			target dab2;
+			while( MEXArray[i].sanic_count != 0 ){
+				dab2.x = MEXArray[i].x;
+				dab2.y = MEXArray[i].y;
+				sanic_speed.push_back( dab2 );
 
 				i = MEXArray[i].back;
 			}
@@ -145,7 +145,7 @@ void FindPath( int sx, int sy, int x, int y ){
 
 int main()
 {
-    bool running = true;
+    bool narutorunning = true;
 	int x = 15;
 	int y = 16;
 	int old_x;
@@ -157,7 +157,7 @@ int main()
 
 	int pts = 0;
 
-	cout<< "Instruction:\n1. Arrow Keys to move Shrek\n2. Eat the dots from Lord Farquaad to gain points\n3. Don't get caught by Lord Farquaad\n\n" << endl;
+	cout<< "Instruction:\n1. Utilze the dank and MLG Arrow Keys to move the shrektakular Shrek\n2. Eat the cash spilling from Lord Farquaad's pockets to gain points and to stop flow of capitalism\n3. Don't get caught by Lord Farquaad in order to keep the ideals of Marx alive, also enter the letters E, N, or H, in caps, to select your game mode\n" << endl;
 	printf("H -> Hard\nN -> Normal\nE -> EZ/Noob level\n\nInput : ");
 
 	char diffi;
@@ -178,9 +178,9 @@ int main()
 
 	int frame = 0;
 
-	FindPath( ex,ey,x,y );
+	FindNemo( ex,ey,x,y );
 
-	while( running ){
+	while( narutorunning ){
 		gotoxy( x, y ); cout << " ";
 
 		old_x = x;
@@ -212,10 +212,10 @@ int main()
 		map[ey][ex] = '.';
 		gotoxy( ex, ey ); cout << ".";
 
-		if( frame%speedmod == 0 && walk_queue.size() != 0 ){
-			ex = walk_queue.back().x;
-			ey = walk_queue.back().y;
-			walk_queue.pop_back();
+		if( frame%speedmod == 0 && sanic_speed.size() != 0 ){
+			ex = sanic_speed.back().x;
+			ey = sanic_speed.back().y;
+			sanic_speed.pop_back();
 		}
 
 		gotoxy( ex, ey ); cout << "F";
@@ -235,13 +235,10 @@ int main()
 	printf("You Lost?!? What the Shrek my dood?!?\nThanks to you, Farquad has taken ogre!\nWay to go, dorc.\nYour dank score is : %i ", pts );
 	cin.get();
 	cin.get();
-	cin.get();
-	cin.get();
-	cin.get();
-	cin.get();
-	cin.get();
-	cin.get();
+
 
 	return 0;
 }
+
+
 
